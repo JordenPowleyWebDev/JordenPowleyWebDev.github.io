bececfa5ce9d6e63104b39de0b59d423f0c17b77
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2018-10-17 21:39:42 +0100

    Style update

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * ------------------------------------------------
+ * Jorden Powly GitHub CV
+ * ------------------------------------------------
+ * Main JS File
+ * @desc        - Contains all primary functions of 
+ *                the site.
+ * @requires    - jQuery
+ * ------------------------------------------------
+ */
+
+/**
+ * On Ready
+ */
+$(document).ready(function(){
+    /**
+     * On load functions 
+     */
+    
+});
+
+/**
+ * nav_traverse()
+ */
