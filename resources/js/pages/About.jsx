64550d8ae365114e413b1fee4067a82201199cd0
JordenPowleyWebDev <jorden.powley.webdev@gmail.com>
2022-03-08 18:34:35 +0000

    Update About.jsx

--- resources/js/pages/About.jsx
+++ resources/js/pages/About.jsx
@@ -22,17 +22,18 @@ export default class About extends React.PureComponent {
                     <Divider />
                     <div className="row m-0 p-0">
                         <div className="col-12 col-md-6 m-0 p-0 pr-md-3 pr-lg-4">
-                            <Title>I am a web developer based around Derby</Title>
-                            <Text>After graduating Keele University in 2016 with a Bachelor's Degree in BSc Computer Science, I spent two years working form Zonal Marketing technologies (formally TXD Limited). Here I worked on many projects ranging from building marketing email templates to gift card e-commerce systems, for clients include New World Trading Company, Butcombe Brewery and All Star Lanes.</Text>
+                            <Title>I am a web developer based in Islington</Title>
+                            <Text>After graduating Keele University in 2016 with a Bachelor's Degree in BSc Computer Science, I spent two years working for Zonal Marketing technologies (formally TXD Limited). Here I worked on many projects ranging from building marketing email templates to gift card e-commerce systems, for clients include New World Trading Company, Butcombe Brewery and All Star Lanes.</Text>
                             <Text>Following my departure from ZMT I worked for The Together Agency. A creative branding agency based in Nottingham. Here I primarily migrated websites and applications to use a Bitbucket pipeline / AWS architecture with a smattering of smaller projects along the line.</Text>
-                            <Text>From October the 21st 2019 I will be working for Langley Foxall on what I believe will be some exciting new projects!</Text>
+                            <Text>Currently I work for Langley Foxall as a Lead Developer. My team and I develop bespoke ERP (Enterprise Resource Planning) and OMS (Order Management System) applications. These are mainly built on Laravel with React used for the more 'reactive' elements of each project.</Text>
                             <Text className={"mb-0"}>Occasionally in my spare time I do smaller development projects for myself, however like the typical developer these are usually far to grand a scale and get put down as soon as I get my next big idea...</Text>
                         </div>
                         <Divider className={"border-red-pink col-12 d-block d-md-none"} />
                         <div className="col-12 col-md-6 m-0 p-0 pl-md-3 pl-lg-4">
                             <Title>Doesn't work 100% of the time</Title>
                             <Text>Being an ever evolving individual, verbose way of saying 'short attention span', my hobbies reflect this.</Text>
-                            <Text className={"mb-0"}>Currently these include kayaking, running, mediocre djing and repairing my (inflatable) kayak.</Text>
+                            <Text>Currently these include kayaking down the rough waves of the Regent's canal (weather dependent), running, mediocre djing and repairing my (inflatable) kayak.</Text>
+                            <Text className={"mb-0"}>Being relatively new to London, I'm on the lookup for random London based activities. So far I've found a cricket alternative to bowling, shuffleboard and a very high tech crazy golf. Suggestions are always accepted...</Text>
                         </div>
                     </div>
                     <Divider />
